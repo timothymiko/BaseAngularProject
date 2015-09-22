27c727e13efb33fdd2750506d9172d42090baf17
Timothy Miko <timothymiko@gmail.com>
2015-09-22 09:06:06 -0400

    Update gulp task for creating a new view.

--- gulpfile.js
+++ gulpfile.js
@@ -13,18 +13,22 @@ function string_src(filename, string) {
 
 gulp.task('view', function(name, url) {
 
-    string_src(name + '.js', 'app.controller(\''+name+'\', function($scope, $state) {\n\n});')
+    string_src(name + '.js', 'app.controller(\''+name+'\', function($scope, $state) {\n\n\t\'user strict\';\n\n});')
         .pipe(gulp.dest('./controllers/'));
 
     string_src(name + '.html', '')
         .pipe(gulp.dest('./partials/'));
 
     gulp.src('index.html')
-        .pipe(inject.after('<!-- VIEW FILES -->', '\n\t<script src="' + name + '.js"></script>'))
+        .pipe(inject.after('<!-- VIEW FILES -->', '\n\t<script src="./controllers/' + name + '.js"></script>'))
         .pipe(gulp.dest('./'));
 
     gulp.src('app.js')
         .pipe(inject.before('// end of view states', '\n\t\t\t.state("'+name+'", { \n\t\t\t\turl: "'+url+'",\n\t\t\t\ttemplateUrl: "./partials/'+name+'.html",\n\t\t\t\tcontroller: "'+name+'"\n\t\t\t})\n\n'))
+        .pipe(gulp.dest('./'));
+
+    gulp.src('app.css')
+        .pipe(inject.append('\n/**\n* ' + name + '\n*/'))
         .pipe(gulp.dest('./'));
 
     return 1;
